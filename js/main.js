1d59afc33427de684d0acd5ef973469be087dc59
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-09-03 19:24:53 +0200

    Remove the anchor id from the link when clicked

--- js/main.js
+++ js/main.js
@@ -13,3 +13,16 @@ $(window).scroll(function() {
         navbar.removeClass("is-fixed-top");
     }
 });
+
+/*
+*
+*	Url rewrite when clicking an anchor.
+*
+*/
+document.querySelectorAll('a').forEach(link => {
+	link.addEventListener('click', () => {
+		setTimeout(() => {
+			history.pushState('', document.title, window.location.pathname);
+		}, 100);
+	});
+});
